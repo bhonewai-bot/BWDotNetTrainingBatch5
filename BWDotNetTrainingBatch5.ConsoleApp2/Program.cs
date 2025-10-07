7e310c41d19a2b46f632e2f3bc66ad0f3ad3dd85
Bhone Wai <damenaosan@gmail.com>
2025-10-07 20:21:21 +0630

    feat: complete console

---
+++ BWDotNetTrainingBatch5.ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetTrainingBatch5.Database.Models;
+
+Console.WriteLine("Hello, World!");
+
+AppDbContext db = new AppDbContext();
+var lts = db.TblBlogs.ToList();
+foreach (var item in lts)
+{
+    Console.WriteLine(item.BlogId);
+    Console.WriteLine(item.BlogTitle);
+    Console.WriteLine(item.BlogAuthor);
+    Console.WriteLine(item.BlogContent);
+}
