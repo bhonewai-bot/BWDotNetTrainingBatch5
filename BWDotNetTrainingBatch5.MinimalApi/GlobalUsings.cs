868f56ecbc1c8224c946d41d8bd8dda15da0140c
Bhone Wai <damenaosan@gmail.com>
2025-10-26 19:26:59 +0630

    feat: add minimal api

---
+++ BWDotNetTrainingBatch5.MinimalApi/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using DotNetTrainingBatch5.Database.Models;
+global using Microsoft.EntityFrameworkCore;
+global using BWDotNetTrainingBatch5.MinimalApi.Endpoints.Blog;
+global using DotNetTrainingBatch5.Database.Models;
