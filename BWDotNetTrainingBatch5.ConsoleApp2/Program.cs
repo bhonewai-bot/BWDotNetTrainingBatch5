abf7f523908c653a60e0161d2dc0210f4f37e016
Bhone Wai <damenaosan@gmail.com>
2025-10-26 19:23:16 +0630

    feat: add JSON serialization and deserialization

--- BWDotNetTrainingBatch5.ConsoleApp2/Program.cs
+++ BWDotNetTrainingBatch5.ConsoleApp2/Program.cs
@@ -1,15 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DotNetTrainingBatch5.Database.Models;
+using Newtonsoft.Json;
 
 Console.WriteLine("Hello, World!");
 
-AppDbContext db = new AppDbContext();
-var lts = db.TblBlogs.ToList();
-foreach (var item in lts)
+var blog = new BlogModel()
 {
-    Console.WriteLine(item.BlogId);
-    Console.WriteLine(item.BlogTitle);
-    Console.WriteLine(item.BlogAuthor);
-    Console.WriteLine(item.BlogContent);
+    Id = 1,
+    Title = "Dutchie",
+    Author = "Bhone Wai",
+    Content = "Yogurt",
+};
+
+string jsonStr = blog.ToJson();
+Console.WriteLine(jsonStr);
+
+string jsonStr2 = """{"Id":1,"Title":"Dutchie","Author":"Bhone Wai","Content":"Yogurt"}""";
+var blog2 = JsonConvert.DeserializeObject<BlogModel>(jsonStr2);
+Console.WriteLine(blog2.Title);
+
+public class BlogModel
+{
+    public int Id { get; set; }
+    
+    public string? Title { get; set; }
+    
+    public string? Author { get; set; }
+    
+    public string? Content { get; set; }
+}
+
+public static class Extensions
+{
+    public static string ToJson(this object obj)
+    {
+        string jsonStr = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        return jsonStr;
+    }
 }
