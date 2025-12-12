2dc9dc660945df8b2a4d7022d6dd1b4111a3287b
Bhone Wai <damenaosan@gmail.com>
2025-12-12 21:06:37 +0630

    feat: candleStick, column and area chart

--- BWDotNetTrainingBatch5.ChartWebApp/Models/ApexChartModel.cs
+++ BWDotNetTrainingBatch5.ChartWebApp/Models/ApexChartModel.cs
@@ -1,11 +1,13 @@
 namespace BWDotNetTrainingBatch5.ChartWebApp.Models;
 
+/* Pie Chart */
 public class ApexChartPieChartModel
 {
     public int[] Series { get; set; }
     public string[] Labels { get; set; } 
 }
 
+/* Mixed Chart */
 public class ApexChartMixedChartModel
 {
     public List<SeriesData> Series { get; set; }
@@ -18,3 +20,48 @@ public class SeriesData
     public string Type { get; set; }
     public int[] Data { get; set; }
 }
+
+/* Candle Stick Chart */
+public class ApexChartCandlestickModel
+{
+    public List<CandlestickSeriesData> CandlestickData { get; set; }
+    public List<LineSeriesData> LineData { get; set; }
+}
+
+public class CandlestickSeriesData
+{
+    public long X { get; set; }
+    public decimal[] Y { get; set; } // [Open, High, Low, Close]
+}
+
+public class LineSeriesData
+{
+    public long X { get; set; }
+    public decimal Y { get; set; }
+}
+
+/* Column Chart */
+public class ColumnChartModel
+{
+    public List<ColumnSeriesData> Series { get; set; }
+    public string[] Categories { get; set; }
+}
+
+public class ColumnSeriesData
+{
+    public string Name { get; set; }
+    public int[] Data { get; set; }
+}
+
+/* Area Chart */
+public class ApexChartAreaChartModel
+{
+    public List<AreaChartData> Data { get; set; }
+}
+
+public class AreaChartData
+{
+    public long Timestamp { get; set; }
+    public decimal Value { get; set; }
+}
+
