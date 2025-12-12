e01917d5d96cf9ad6e700f6f9de0b96e860f2e85
Bhone Wai <damenaosan@gmail.com>
2025-12-12 17:04:38 +0630

    feat: ASP.NET Core MVC using ApexChart

---
+++ BWDotNetTrainingBatch5.ChartWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BWDotNetTrainingBatch5.ChartWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
